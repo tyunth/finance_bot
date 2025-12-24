8b5e022f9760e282927b558d43207d777dd3ff89
tyunth <polandszymon@gmail.com>
2025-12-24 22:03:50 +0500

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { google } = require('googleapis');
 const path = require('path');
 const config = require('./config');
@@ -113,7 +114,7 @@ function parseLessonInfo(summary) {
 
 // Получить события за конкретный день (весь день)
 async function getEventsForDate(dateObj) {
-    const CALENDAR_ID = process.env.GOOGLE_CALENDAR_ID || 'polandszymon@gmail.com';
+    const CALENDAR_ID = process.env.GOOGLE_CALENDAR_ID';
     try {
         const start = new Date(dateObj);
         start.setHours(0, 0, 0, 0);
