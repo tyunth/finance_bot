ef41c1c2f633e77fe37699c56e3da1043c608fd2
tyunth <polandszymon@gmail.com>
2025-12-26 20:17:04 +0500

    Create menu.js

---
+++ modules/utilities/menu.js
@@ -0,0 +1,27 @@
+const { Markup } = require('telegraf');
+const db = require('../db');
+
+async function getMainMenu(userId) {
+    const modules = await db.getUserModules(userId);
+    
+    // 1. Базовые
+    const buttons = [['📉 Расходы', '📈 Доходы']];
+    
+    // 2. Ученики / Календарь
+    if (modules.includes('all') || modules.includes('students')) {
+        buttons.push(['🎓 Ученики', '📅 Расписание']);
+    }
+    
+    // 3. Спорт
+    if (modules.includes('all') || modules.includes('sport')) {
+        buttons.push(['💪 Спорт']);
+    }
+    
+    // 4. Остальное
+    buttons.push(['📊 Отчет', 'Счета']);
+    buttons.push(['Помощь']);
+
+    return Markup.keyboard(buttons).resize();
+}
+
+module.exports = { getMainMenu };
