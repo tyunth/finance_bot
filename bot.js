cd723178917a817a364efd3432d96a4edc5c8921
tyunth <polandszymon@gmail.com>
2025-12-26 20:36:40 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const { Telegraf, session, Markup } = require('telegraf');
 const config = require('./config');
 const db = require('./db');
-const { getMainMenu } = require('./utilities/menu.js');
+const { getMainMenu } = require('./modules/utilities/menu.js');
 
 // Инициализация
 const bot = new Telegraf(process.env.BOT_TOKEN);
