05b6995cc1655eb3dd85f1e3b0c71bbfef55ad34
tyunth <polandszymon@gmail.com>
2025-12-26 21:09:13 +0500

    Update menu.js

--- modules/utilities/menu.js
+++ modules/utilities/menu.js
@@ -1,27 +1,34 @@
 const { Markup } = require('telegraf');
 const db = require('../../db');
-
 async function getMainMenu(userId) {
-    const modules = await db.getUserModules(userId);
+    // Твоя любимая раскладка
+    return Markup.keyboard([
+        ['Доход', 'Расход', 'Перевод'],
+        ['Счета', 'Отчеты', 'Помощь'],
+        ['Список', 'Дела', 'Вишлист']
+    ]).resize();
+}
+//async function getMainMenu(userId) {
+ //   const modules = await db.getUserModules(userId);
     
     // 1. Базовые
-    const buttons = [['📉 Расходы', '📈 Доходы']];
+  //  const buttons = [['📉 Расходы', '📈 Доходы']];
     
     // 2. Ученики / Календарь
-    if (modules.includes('all') || modules.includes('students')) {
-        buttons.push(['🎓 Ученики', '📅 Расписание']);
-    }
+   // if (modules.includes('all') || modules.includes('students')) {
+  //      buttons.push(['🎓 Ученики', '📅 Расписание']);
+ //   }
     
     // 3. Спорт
-    if (modules.includes('all') || modules.includes('sport')) {
-        buttons.push(['💪 Спорт']);
-    }
+ //   if (modules.includes('all') || modules.includes('sport')) {
+//        buttons.push(['💪 Спорт']);
+ //   }
     
     // 4. Остальное
-    buttons.push(['📊 Отчет', 'Счета']);
-    buttons.push(['Помощь']);
+ //   buttons.push(['📊 Отчет', 'Счета']);
+ //   buttons.push(['Помощь']);
 
-    return Markup.keyboard(buttons).resize();
-}
+ //   return Markup.keyboard(buttons).resize();
+//}
 
 module.exports = { getMainMenu };
